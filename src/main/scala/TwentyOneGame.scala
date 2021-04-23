e9c8bd34052af97e4dd0806666d91231b2d2cae2
menno4000 <maxluedemann@t-online.de>
2021-04-23 15:13:46 +0200

    blackjack and hookahs

--- src/main/scala/TwentyOneGame.scala
+++ src/main/scala/TwentyOneGame.scala
@@ -1,3 +1,5 @@
+import TaskOneOperations.max, TaskOneOperations.min
+
 object TwentyOneGame {
   def arrayToString[X](x: X): String = x match {
     case arr: Array[_] => arr.map(arrayToString).mkString("[", ", ", "]")
@@ -23,6 +25,43 @@ object TwentyOneGame {
     case 11 => Array(1, 11)
     case _ => Array(card)
   }
+  def isBust(handValue: Int): Boolean = handValue match {
+    case x if x > 21 => true
+    case _ => false
+  }
+
+  // simplified method
+  def determineHandValue (strategy:Array[Int]=>Int)(hand: Array[Int]): Int =
+    hand.map(x => strategy(values(x))).sum
+
+  def optimisticF: Array[Int] => Int = determineHandValue(max)
+  def pessimisticF: Array[Int] => Int = determineHandValue(min)
+
+
+  def determineBestHandValue(hand:Array[Int]):Int = optimisticF(hand) match {
+    case x if isBust(x) => pessimisticF(hand)
+    case other => other
+  }
+
+  // context conditional Method
+  def contextF(hand:Array[Int], eleven:Boolean, bust:Boolean): Int = if (bust) {
+    pessimisticF(hand)
+  } else {
+    if (eleven) {
+      pessimisticF(hand) + 10
+    } else {
+      optimisticF(hand)
+    }
+  }
+
+  def determineBetterHandValue(hand: Array[Int]): Int = contextF(hand, eleven = false, bust = false) match {
+    case x1 if isBust(x1) => contextF(hand, eleven = true, bust = false ) match {
+      case x2 if isBust(x2) => contextF(hand, eleven = true, bust = true)
+      case other => other
+    }
+    case other => other
+  }
+
 
   def main(args: Array[String]): Unit = {
     println(arrayToString(parse("2")))
